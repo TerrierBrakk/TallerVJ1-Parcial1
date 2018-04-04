e620c6bac9f73e0cb0ef22cdf19cebd70368b51f
TerrierBrakk <32682025+TerrierBrakk@users.noreply.github.com>
2018-04-04 02:02:02 -0500

    Inicios Movimiento
    add script de movimiento

---
+++ Proyecto/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		Movimiento ();
+		Salto ();
+	}
+
+	void Movimiento()
+	{
+		var mov = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+		transform.Translate(mov, 0, 0);
+	}
+
+	void Salto()
+	{
+		var salto = Input.GetAxis ("Vertical") * Time.deltaTime * 40.0f;
+
+
+			transform.Translate (0, salto, 0);
+	}
+}
