025b860caff2995b1333a4b5526d29f246547334
TerrierBrakk <32682025+TerrierBrakk@users.noreply.github.com>
2018-04-18 06:19:08 -0500

    Control de personaje
    Ya puede brincar solo tocado suelo
    configurado para teclado y mando

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,23 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
+	
+
+	private Rigidbody rb;
+	Animator anim;
+	private float isground =0.2f;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();	
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
 		Movimiento ();
-	}
-
-	void Movimiento()
-	{
-		var mov = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
-		transform.Translate(mov, 0, 0);
+		saltar ();
 	}
 
 
-}
+	void Movimiento() ///funcion de movimiento en el eje x
+		{
+			float mov = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+			transform.Translate(mov, 0, 0);
+		}
+
+	void saltar() // Funcion para saltar
+		{
+			Debug.Log (isGrounded ());
+
+			if (Input.GetButtonDown ("Jump")&&isGrounded())
+			{
+				
+				rb.velocity = new Vector3 (0, 4, 0);
+			}		
+		}
+		
+	bool isGrounded() //Funcion que lanza un raycast hacia abajo para checar suelo
+		{
+			return Physics.Raycast (transform.position, Vector3.down, isground);
+		}
+	}
+
