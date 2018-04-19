149c481795d0c230fa62b8a40e1adffd9fceb7db
Lenin Figueroa <lenin.figa@gmail.com>
2018-04-18 22:12:28 -0500

    PauseMenu
    a la escena de prueba se le agrego el menu de pausa, ojo, tambien en el canvas viene la pantalla de game over para que no se confundan con esa, por ahora pausa se pone con la letra P

--- Assets/PruebasLenin/PoolManager/spawner.cs
+++ Assets/PruebasLenin/PoolManager/spawner.cs
@@ -7,14 +7,25 @@ public class spawner : MonoBehaviour {
 	pooler objectPooler;
 	float tiempo_generar;
 	public float Frecuencia = 1f;
+    Vector3 spawnInicial;
+    float DistanciaZ = 3.1f;
 
 	private void Start()
 	{
 		objectPooler = pooler.Instance;
-		tiempo_generar = Time.time + 1f;
-	}
+		tiempo_generar = Time.time + Frecuencia;
+        spawnInicial = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        for (int i = 0; i < 7; i++)
+        {
+            objectPooler.SpawnFromPool("obstaculo", spawnInicial, Quaternion.identity);
+            spawnInicial.z -= DistanciaZ;
+
+        }
+    }
 
 	void FixedUpdate() {
+
+       
 		
 	
 		if (tiempo_generar < Time.time) {
